b505392723e0442a5ee4b48a1754b9324eb0c034
Aristo <33140521+surjadhanaAristo@users.noreply.github.com>
2024-01-27 14:06:34 -0500

    Finished increasing sub

--- main.py
+++ main.py
@@ -42,6 +42,67 @@ def mergeSort(arr):
             arr[k] = R[j]
             j += 1
             k += 1
+    
+    return arr
+def increasingSub(str):
+    ind_1 = 0
+    ind_2 = 0
+    counter =  0
+    prev = str[0]
+    flag = False
+    size = [0,0]
+    for x in str:
+        if(flag == False):
+            ind_1 = counter - 1
+        if (prev < x):
+            ind_2 = counter + 1
+            flag = True
+        else:
+            if (flag == True and ind_2 - ind_1 > size[1] - size[0]):
+                size[0] = ind_1
+                size[1] = ind_2
+                ind_1 = 0
+                ind_2 = 0
+                flag = False
+            else:
+                ind_1 = 0
+                ind_2 = 0
+                flag = False
+
+        prev = x
+        counter += 1
+    if (flag == True and ind_2 - ind_1 > size[1] - size[0]):
+                size[0] = ind_1
+                size[1] = ind_2
+                ind_1 = 0
+                ind_2 = 0
+    return size
+def commonSub(str1, str2):
+    size = [0, 0]
+    ind_1 = 0
+    ind_2 = 0
+    counter = 0
+    flag = False
+    for x in str2:
+        try:
+            if (str1.index(x) > 0):
+                ind_2 = str1.index1(x)
+                ind_1 = str1.index1(x)
+                count2 = counter
+                while (str2[counter] == str1[ind_2]):
+                    counter += 1
+                    ind_2 += 1
+                counter = count2
+                if(ind_2 - ind_1 > size[1] - size[0]):
+                    size[0] = ind_1
+                    size[1] = ind_2
+                    ind_1 = 0
+                    ind_2 = 0
+            break
+        except ValueError:
+            ind = 0
+        counter += 1
+
 
 
 
@@ -49,6 +110,8 @@ def main():
     #readJson('/Users/asurj/Documents/GitHub/datascienceprep/MOCK_DATA.json')
     arr = [3 ,5 ,9 , 2, 6, 4, 7, 2, 9, 1, 3, 6, 2, 8]
     arr = mergeSort(arr)
+    str = "hellabcao"
+    print(increasingSub(str))
     print(arr)
 
 
